4991cfa7716972fc86c2b2b57c5f975885056160
Sander Mirck <sandermirck@gmail.com>
2026-08-25 17:54:55 +0200

    remove terminal rice

--- configuration.nix
+++ configuration.nix
@@ -76,17 +76,6 @@
     };
   };
 
-  # XFCE terminal met Gruvbox kleuren
-  programs.xfconf = {
-    enable = true;
-    settings = {
-      "xfce4-terminal" = {
-        "color-foreground" = "#ebdbb2";
-        "color-background" = "#282828";
-        "color-palette" = ''#282828;#cc241d;#98971a;#d79921;#458588;#b16286;#689d6a;#a89984;#928374;#fb4934;#b8bb26;#fabd2f;#83a598;#d3869b;#8ec07c;#ebdbb2'';
-      };
-    };
-  };
 
 
   # ---- PRINTEN ----
--- neovim.nix
+++ neovim.nix
@@ -6,18 +6,16 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      gruvbox
-      # voeg hier andere plugins toe
-    ];
-    extraConfig = ''
-      " Gruvbox instellingen
-      colorscheme gruvbox
-      set background=dark
-      
-      " Optionele Gruvbox tweaks
-      let g:gruvbox_contrast_dark = 'hard'
-      let g:gruvbox_invert_selection = '0'
-    '';
+    configure = {
+      customRC = ''
+        colorscheme gruvbox
+        set background=dark
+        let g:gruvbox_contrast_dark = 'hard'
+        let g:gruvbox_invert_selection = '0'
+      '';
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [ gruvbox ];
+      };
+    };
   };
 }
